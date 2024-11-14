735714626b9a04b29b64f439279d74f3b8337c66
Leigh Goetsch <goetschm@msoe.edu>
2024-11-14 16:22:16 -0600

    info and pinfo programs

--- labs/lab9/system-calls-turney-jeffTheLandShark/info.c
+++ labs/lab9/system-calls-turney-jeffTheLandShark/info.c
@@ -1,9 +1,49 @@
-// Name, etc
+/*
+ * @file info.c
+ * @brief Prints system information
+ */
 
-// info.c
+#include <stdio.h>
+#include <sys/sysinfo.h>
+#include <sys/utsname.h>
+#include <time.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[])
-{
+int main() {
+  struct timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
 
-    return 0;
+  printf("Example of system information\n\n");
+  printf("1. The current time of day - in nanoseconds\n");
+  printf("Current time: %.2f nanoseconds\n",
+         ts.tv_nsec + (ts.tv_sec * 1000000000.0));
+  printf("\n");
+
+  struct utsname uts;
+  uname(&uts);
+
+  printf("2. The system's network name\n");
+  printf("Network name: %s\n", uts.nodename);
+  printf("\n");
+  printf("3. The operating system name\n");
+  printf("Operating system: %s\n", uts.sysname);
+  printf("\n");
+  printf("4. The operating system release and version\n");
+  printf("Release: %s\n", uts.release);
+  printf("\n");
+  printf("5. The system's hardware type\n");
+  printf("Hardware type: %s\n", uts.machine);
+  printf("\n");
+  printf("6. The number of CPUs on the system\n");
+  printf("Number of CPUs: %d\n", get_nprocs());
+  printf("\n");
+  printf("7. The total amount of physical memory IN BYTES\n");
+  printf("Total memory: %ld\n", get_phys_pages() * getpagesize());
+  printf("\n");
+  printf("8. The total amount of free memory IN BYTES\n");
+  struct sysinfo si;
+  sysinfo(&si);
+  printf("Free memory: %ld\n", si.freeram);
+
+  return 0;
 }
--- labs/lab9/system-calls-turney-jeffTheLandShark/pinfo.c
+++ labs/lab9/system-calls-turney-jeffTheLandShark/pinfo.c
@@ -1,9 +1,77 @@
-// Name, etc
+/*
+ * @file pinfo.c
+ * @brief Prints system information
+ */
 
-// pinfo.c
+#include <unistd.h>
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/resource.h>
 
-int main(int argc, char* argv[])
-{
+    int main(int argc, char *argv[]) {
+  // - Take a single command line parameter that is a process identifier
+  // - For that process identifier, print out
+  // - The process priority
+  // - The scheduling method - as a string of text NOT a number
+  // - If the process identifier does not exist, print an error message and exit
+  // - If the user does NOT specify a process identifier, print the information
+  // for the CURRENT executing process
 
-    return 0;
+  if (argc == 1) {
+    // Print information for the current process
+    pid_t pid = getpid();
+    int policy = sched_getscheduler(pid);
+    char *policy_str;
+    switch (policy) {
+    case SCHED_OTHER:
+      policy_str = "SCHED_OTHER";
+      break;
+    case SCHED_FIFO:
+      policy_str = "SCHED_FIFO";
+      break;
+    case SCHED_RR:
+      policy_str = "SCHED_RR";
+      break;
+    default:
+      policy_str = "UNKNOWN";
+      break;
+    }
+
+    printf("Results for current process:\n");
+    printf("Priority: %d\n", getpriority(PRIO_PROCESS, 0));
+    printf("Scheduling method: %s\n", policy_str);
+  } else if (argc == 2) {
+    // Print information for the specified process
+    int pid = atoi(argv[1]);
+    int policy = sched_getscheduler(pid);
+    if (policy == -1) {
+      perror("sched_getscheduler");
+      return 1;
+    }
+
+    char *policy_str;
+    switch (policy) {
+    case SCHED_OTHER:
+      policy_str = "SCHED_OTHER";
+      break;
+    case SCHED_FIFO:
+      policy_str = "SCHED_FIFO";
+      break;
+    case SCHED_RR:
+      policy_str = "SCHED_RR";
+      break;
+    default:
+      policy_str = "UNKNOWN";
+      break;
+    }
+
+    printf("Results for process %d:\n", pid);
+    printf("Priority: %d\n", getpriority(PRIO_PROCESS, pid));
+    printf("Scheduling method: %s\n", policy_str);
+  } else {
+    fprintf(stderr, "Usage: %s [pid]\n", argv[0]);
+    return 1;
+  }
 }
