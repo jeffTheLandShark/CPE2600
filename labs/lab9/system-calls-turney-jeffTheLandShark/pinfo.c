df66d536326f2474107eee734e21ae4e78cbe8e7
Leigh Goetsch <goetschm@msoe.edu>
2024-11-14 23:30:34 -0600

    update comments

--- labs/lab9/system-calls-turney-jeffTheLandShark/pinfo.c
+++ labs/lab9/system-calls-turney-jeffTheLandShark/pinfo.c
@@ -1,25 +1,18 @@
 /*
  * @file pinfo.c
- * @brief Prints system information
+ * @brief Prints the priority and scheduling method of a process
  */
 
-#include <unistd.h>
 #define _GNU_SOURCE
 #include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/resource.h>
+#include <unistd.h>
 
 void print_pinfo(pid_t pid);
 
 int main(int argc, char *argv[]) {
-  // - Take a single command line parameter that is a process identifier
-  // - For that process identifier, print out
-  // - The process priority
-  // - The scheduling method - as a string of text NOT a number
-  // - If the process identifier does not exist, print an error message and exit
-  // - If the user does NOT specify a process identifier, print the information
-  // for the CURRENT executing process
 
   if (argc == 1) {
     // Print information for the current process
@@ -37,6 +30,10 @@ int main(int argc, char *argv[]) {
 }
 
 void print_pinfo(pid_t pid) {
+  // get priority
+  int priority = getpriority(PRIO_PROCESS, pid);
+
+  // get scheduling policy
   int policy = sched_getscheduler(pid);
 
   if (policy == -1) {
@@ -55,12 +52,24 @@ void print_pinfo(pid_t pid) {
   case SCHED_RR:
     policy_str = "SCHED_RR";
     break;
+  case SCHED_BATCH:
+    policy_str = "SCHED_BATCH";
+    break;
+  case SCHED_ISO:
+    policy_str = "SCHED_ISO";
+    break;
+  case SCHED_IDLE:
+    policy_str = "SCHED_IDLE";
+    break;
+  case SCHED_DEADLINE:
+    policy_str = "SCHED_DEADLINE";
+    break;
   default:
     policy_str = "UNKNOWN";
     break;
   }
 
   printf("Results for process %d:\n", pid);
-  printf("Priority: %d\n", getpriority(PRIO_PROCESS, pid));
+  printf("Priority: %d\n", priority);
   printf("Scheduling method: %s\n", policy_str);
 }
