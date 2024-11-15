c576d766a6206ffac73e0277413ab597df413180
Leigh Goetsch <goetschm@msoe.edu>
2024-11-14 23:31:04 -0600

    pmod

--- labs/lab9/system-calls-turney-jeffTheLandShark/pmod.c
+++ labs/lab9/system-calls-turney-jeffTheLandShark/pmod.c
@@ -1,9 +1,22 @@
-// Name, etc
+/*
+ * @file pmod.c
+ * @brief This program modifies its own priority to reduce it by 10, sleeps for
+ * 1,837,272,638 nano seconds, prints a goodbye message, and exits.
+ */
 
-// pmod.c
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
+  nice(10);
+  
+  struct timespec ts;
+  ts.tv_sec = 1;
+  ts.tv_nsec = 837272638;
+  nanosleep(&ts, NULL);
 
-    return 0;
+  printf("Goodbye!\n");
+
+  return 0;
 }
