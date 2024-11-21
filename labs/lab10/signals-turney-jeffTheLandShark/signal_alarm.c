a7f2cfb69be60c11a0f3029ac8ce542ca9a09d74
jeffTheLandShark <goetschm@msoe.edu>
2024-11-21 16:32:28 -0600

    create signal alarm program (part 3)

--- labs/lab10/signals-turney-jeffTheLandShark/signal_alarm.c
+++ labs/lab10/signals-turney-jeffTheLandShark/signal_alarm.c
@@ -1,6 +1,33 @@
 /**
- * File: 
- * Modified by:
- * 
- * Brief summary of program:
+ * @file signal_alarm.c
+ * Modified by: Leigh Goetsch
+ *
+ * @brief Schedules an alarm to go off in 5 seconds.
+ * has signal handler to print that message was received.
  */
+
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * @brief Signal handler for SIGALRM - prints a message
+ */
+void handle_signal() { printf("Received SIGALRM signal\n"); }
+
+int main() {
+
+  // Register for the signal
+  signal(SIGALRM, handle_signal);
+
+  // Schedule an alarm to go off in 5 seconds
+  alarm(5);
+
+  // Wait until a signal is received
+  while (1) {
+    printf("Sleeping\n");
+    sleep(1);
+  }
+
+  return 0;
+}
