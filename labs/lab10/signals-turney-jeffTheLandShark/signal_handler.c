cec5d4213996e0faa591574570173474dca7dfdb
jeffTheLandShark <goetschm@msoe.edu>
2024-11-21 16:25:00 -0600

    update handler to not exit on SIGINT

--- labs/lab10/signals-turney-jeffTheLandShark/signal_handler.c
+++ labs/lab10/signals-turney-jeffTheLandShark/signal_handler.c
@@ -1,38 +1,38 @@
 /**
  * @file signal_handler.c
- * @brief Sets a signal handler for SIGINT, the handler prints a message and then quits
+ * @brief Sets a signal handler for SIGINT, the handler prints a message and
+ * then quits
  */
 
 /**
- * Modified by:
- * 
+ * Modified by: Leigh Goetsch
+ *
  * Brief summary of modifications:
+ * 1. removed the exit(0) from the handle_signal function
+ * 2. updated printf statement to the handle_signal function
  */
 
-
 #include <signal.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * @brief Signal handler for SIGINT - prints a message and exits
  */
 void handle_signal() {
-    printf("Received a signal\n");
-    exit(1);
+  printf("Received SIGINT signal, but continuing execution\n");
 }
 
 int main() {
 
-    // Register for the signal
-    signal(SIGINT, handle_signal);
+  // Register for the signal
+  signal(SIGINT, handle_signal);
 
-    // Wait until a signal is received
-    while(1) {
-        printf("Sleeping\n");
-        sleep(1);
-    }
+  // Wait until a signal is received
+  while (1) {
+    printf("Sleeping\n");
+    sleep(1);
+  }
 
-    return 0;
+  return 0;
 }
