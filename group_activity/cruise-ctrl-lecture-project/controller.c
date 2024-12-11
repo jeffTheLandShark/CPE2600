9cef9ef305266d9cfee55c6bae1513b4a1f91ae4
jeffTheLandShark <goetschm@msoe.edu>
2024-12-11 16:52:00 -0600

    comment code

--- group_activity/cruise-ctrl-lecture-project/controller.c
+++ group_activity/cruise-ctrl-lecture-project/controller.c
@@ -1,27 +1,36 @@
+/**
+ * @file controller.c
+ * @brief controller for the cruise control system
+ *
+ * @author group 3
+ * @date 12/11/2024
+ */
 #include <math.h>
 
-void controller(int cruise, double speed_setp, double actual_speed, double position_setp,
-double actual_position, int manual_accel, int manual_break, int* command_accel, int* command_break) {
-    if (cruise && !manual_accel && !manual_break) { //active
-        if (actual_position - position_setp < 0) { //car is too close
-            *command_accel = 0;
-            *command_break = 25 + (int) (100.0 / (position - position_setp));
+void controller(int cruise, double speed_setp, double actual_speed,
+                double position_setp, double actual_position, int manual_accel,
+                int manual_break, int *command_accel, int *command_break) {
+  if (cruise && !manual_accel && !manual_break) { // active
 
-            if(*command_break > 100) {
-                *command_break = 100;
-            }
-        } else if (abs(actual_speed - speed_setp) > 3) { //cruise control will get within 5 mph
-            int percent = (abs(actual_speed - speed_setp) - 3) * 5;
-            if (actual_speed - speed_setp > 0) { //too fast
-                *command_break = percent;
-                *command_accel = 0;
-            } else { //too slow
-                *command_accel = percent;
-                *command_break = 0;
-            }
+    if (actual_position - position_setp < 0) { // car is too close
+      *command_accel = 0;
+      *command_break = 25 + (int)(100.0 / (actual_position - position_setp));
+
+      if (*command_break > 100) {
+        *command_break = 100;
+      } else if (fabs(actual_speed - speed_setp) >
+                 3) { // cruise control will get within 5 mph
+        int percent = (fabs(actual_speed - speed_setp) - 3) * 5;
+        if (actual_speed - speed_setp > 0) { // too fast
+          *command_break = percent;
+          *command_accel = 0;
+        } else { // too slow
+          *command_accel = percent;
+          *command_break = 0;
         }
+      }
     } else {
-        *command_accel = manual_accel;
-        *command_break = manual_break;
+      *command_accel = manual_accel;
+      *command_break = manual_break;
     }
-}
+  }
