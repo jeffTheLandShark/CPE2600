e2da33588a48d32a068dae27e858a16f231780b0
holden <trumanh@msoe.edu>
2024-12-12 16:07:09 -0600

    adding header and fixing issue

--- group_activity/cruise-ctrl-lecture-project/controller.c
+++ group_activity/cruise-ctrl-lecture-project/controller.c
@@ -1,3 +1,8 @@
+/*
+ * @file controller.h
+ * @brief controller for the cruise control system
+ */
+
 #include <math.h>
 
 void controller(int cruise, double speed_setp, double actual_speed, double position_setp,
@@ -5,14 +10,14 @@ double actual_position, int manual_accel, int manual_break, int* command_accel, 
     if (cruise && !manual_accel && !manual_break) { //active
         if (actual_position - position_setp < 0) { //car is too close
             *command_accel = 0;
-            *command_break = 25 + (int) (100.0 / (actual_position - position_setp));
+            *command_break = 25 + (int) (100.0 / (position_setp - actual_position));
 
             if(*command_break > 100) {
                 *command_break = 100;
             }
         } else if (abs(actual_speed - speed_setp) > 3) { //cruise control will get within 5 mph
             int percent = (fabs(actual_speed - speed_setp) - 3) * 5;
-            if(percent > 100) {
+            if (percent > 100) {
                 percent = 100;
             }
             if (actual_speed - speed_setp > 0) { //too fast
