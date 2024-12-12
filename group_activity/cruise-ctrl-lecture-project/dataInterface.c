ff1e5129bc6425e49de10cd1ac46d40ff5fe1de2
JustinManXx <147108062+JustinManXx@users.noreply.github.com>
2024-12-12 16:10:12 -0600

    Update dataInterface.c

--- group_activity/cruise-ctrl-lecture-project/dataInterface.c
+++ group_activity/cruise-ctrl-lecture-project/dataInterface.c
@@ -4,17 +4,20 @@
 int convertADC(float positionVoltage, float speedVoltage,float *adcFeetPosition,float *adcMPHSpeed);
 float convertDAC(float accelerationPercentage, float breakPercentage, float *accelerationVoltage,float *breakVoltage);
 
+#define MAXINTVALUE 1023
+
 // Function definitions
 int convertADC(float positionVoltage, float speedVoltage,float *adcFeetPosition,float *adcMPHSpeed) {
-    int adcpositionValue = (int)((positionVoltage / 5.0) * 1023);
-    int adcSpeedValue = (int)((speedVoltage / 5.0) * 1023);
-    *adcFeetPosition = (adcpositionValue/1023) * 100;
-    *adcMPHSpeed = (adcSpeedValue/1023) * 100;
+    int adcpositionValue = (int)((positionVoltage / 5.0) * MAXINTVALUE); // creates a voltage proportion and multiplies that by max int value
+    int adcSpeedValue = (int)((speedVoltage / 5.0) * MAXINTVALUE); // creates a voltage proportion and multiplies that by max int value
+    *adcFeetPosition = (adcpositionValue/MAXINTVALUE) * 100; // converts the 10 bit int to a range of 0-100 feet
+    *adcMPHSpeed = (adcSpeedValue/MAXINTVALUE) * 100; // converts the 10 bit int to a range of 0-100 mph
     return 0;
 }
 
 float convertDAC(float accelerationPercentage, float breakPercentage, float *accelerationVoltage,float *breakVoltage) {
-    *accelerationVoltage = accelerationPercentage/100.0 * 10.0;
-    *breakVoltage = breakPercentage/100.0 * 10.0;
+    *accelerationVoltage = accelerationPercentage/100.0 * 10.0; // takes the percentage and converts it to a voltage(0-10V)
+    *breakVoltage = breakPercentage/100.0 * 10.0; // takes the percentage and converts it to a voltage(0-10V)
     return 0;
 }
+
